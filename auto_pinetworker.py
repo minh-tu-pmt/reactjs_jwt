d2be61ec8181bb1a3af373fa9bfab8a095400ad7
minh-tu-pmt <100430471+minh-tu-pmt@users.noreply.github.com>
2022-03-17 09:43:50 +0700

    Update auto_pinetworker.py

--- auto_pinetworker.py
+++ auto_pinetworker.py
@@ -1,5 +1,7 @@
 import sys
 import re
+
+from PyQt5.QtGui import QIntValidator
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
@@ -12,33 +14,6 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QFormLay
 
 list_except=["/users", "/login", "/logout", "/sign"]
 
-def auto(url, NUM_POST, NUM_SCROLL=10):
-    driver = webdriver.Chrome(executable_path='./chromedriver.exe')
-    # url = "https://stackoverflow.com/questions/28431765/open-web-in-new-tab-selenium-python"
-    driver.get(url)
-
-    links = driver.find_elements_by_xpath("//a[@href]")
-    links = [link.get_attribute("href") for link in links if link.get_attribute("href") is not None]
-
-    links = [link for link in links if re.match(r"^(https|http)://[(a-z).]+/[a-z]+/", link)]
-    links = [link for link in links if not any(word in link for word in list_except)]
-
-    links = set(links)
-
-    def get_height():
-        return driver.execute_script("return document.body.scrollHeight")
-
-    count = 0
-    for link in links:
-        if count == NUM_POST:
-            break
-        count += 1
-        driver.get(link)
-        WebDriverWait(driver, 10).until(lambda driver: get_height())
-        for num in range(NUM_SCROLL):
-            driver.execute_script("window.scrollTo(0, {});".format((num + 1) * 100))
-            time.sleep(0.5)
-
 class WindowFrm(QWidget):
     def __init__(self):
         super().__init__()
@@ -47,7 +22,9 @@ class WindowFrm(QWidget):
         self.txtLink = QLineEdit()
         self.cbAnonymus = QCheckBox()
         self.txtQuantity = QLineEdit()
+        self.txtQuantity.setValidator(QIntValidator())
         self.txtTime = QLineEdit()
+        self.txtTime.setValidator(QIntValidator())
 
         outerLayout = QVBoxLayout()
         topLayout = QFormLayout()
@@ -70,8 +47,8 @@ class WindowFrm(QWidget):
         centerLayout.addLayout(time)
         centerLayout.addWidget(self.runBtn)
 
-        txt = QPlainTextEdit()
-        bottomLayout.addWidget(txt)
+        self.txtHistory = QPlainTextEdit()
+        bottomLayout.addWidget(self.txtHistory)
 
         outerLayout.addLayout(topLayout)
         outerLayout.addLayout(centerLayout)
@@ -81,13 +58,57 @@ class WindowFrm(QWidget):
         self.setLayout(outerLayout)
 
     def runBtnClick(self):
-        print("Run click", self.txtLink.text())
         isAnonymus = self.cbAnonymus.isChecked()
-        print("is check: ", isAnonymus)
-        # self.runBtn.setDisabled(True)
         num_post= int(self.txtQuantity.text())
+        timeAlive = int(self.txtTime.text())
         url=self.txtLink.text()
-        auto(url, num_post)
+        self.auto(url,timeAlive,mode=isAnonymus,NUM_POST=num_post)
+
+    def auto(self, url, timeAlive, mode=False, NUM_POST=1):
+        TIME_OUT_SCROLL = 0.5 #seconds
+        options = webdriver.ChromeOptions()
+
+        if mode:
+            options.add_argument("--incognito")
+
+        driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=options)
+
+        # url = "https://stackoverflow.com/questions/28431765/open-web-in-new-tab-selenium-python"
+        driver.get(url)
+
+        links = driver.find_elements_by_xpath("//a[@href]")
+        links = [link.get_attribute("href") for link in links if link.get_attribute("href") is not None]
+
+        links = [link for link in links if re.match(r"^(https|http)://[(a-z).]+/[a-z]+/", link)]
+        links = [link for link in links if not any(word in link for word in list_except)]
+
+        links = set(links)
+        links = list(links)
+
+        links=links[:NUM_POST]
+        strHistory = '---History from '+url+' ---\n'
+        for index in range(len(links)):
+            strHistory+= str(index+1)+'. '+links[index]+'\n'
+
+        self.txtHistory.setPlainText(strHistory)
+        print(strHistory)
+
+        def get_height():
+            return driver.execute_script("return document.body.scrollHeight")
+
+        for link in links:
+            # body = driver.find_element_by_tag_name("body")
+            # body.send_keys(Keys.CONTROL + 't')
+            driver.get(link)
+            WebDriverWait(driver, 10).until(lambda driver: get_height())
+            NUM_SCROLL = round(timeAlive/TIME_OUT_SCROLL)
+            offset = round(get_height()/NUM_SCROLL)
+            for num in range(NUM_SCROLL):
+                driver.execute_script("window.scrollTo(0, {});".format((num + 1) * offset))
+                time.sleep(TIME_OUT_SCROLL)
+
+        driver.close()
+
 
 def run():
     app = QApplication(sys.argv)
